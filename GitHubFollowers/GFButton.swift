9d94e255ad269c5f7a01654822f6a880a53c4a67
Yash <yash.shah@xdesign.com>
2024-02-19 14:31:07 +0000

    Add Reusable Button & TextField

---
+++ GitHubFollowers/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Yash Shah on 19/02/2024.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		configure()
+	}
+	
+	@available(*, unavailable)
+	required init?(coder: NSCoder) {
+		fatalError("init?(coder: NSCoder) has not been implemented")
+	}
+	
+	init(title: String, backgroundColor: UIColor) {
+		super.init(frame: .zero)
+		self.setTitle(title, for: .normal)
+		self.backgroundColor = backgroundColor
+		configure()
+	}
+	
+	private func configure() {
+		translatesAutoresizingMaskIntoConstraints = false
+		layer.cornerRadius = 10
+		titleLabel?.textColor = .white
+		titleLabel?.font = .preferredFont(forTextStyle: .headline)
+	}
+}
