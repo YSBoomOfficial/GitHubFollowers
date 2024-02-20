1ca44addab2ea9d42c294332c41072f91221e8cb
Yash <yash.shah@xdesign.com>
2024-02-20 16:18:04 +0000

    Add User and Follower Models

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Yash Shah on 20/02/2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+	let login: String
+	let avatarUrl: String
+}
---
+++ GitHubFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by Yash Shah on 20/02/2024.
+//
+
+import Foundation
+
+struct User: Codable {
+	let login: String
+	let avatarUrl: String
+	let name: String?
+	let location: String?
+	let bio: String?
+	let publicGists: Int
+	let publicRepos: Int
+	let htmlUrl: String
+	let following: Int
+	let followers: Int
+	let createdAt: String
+}
